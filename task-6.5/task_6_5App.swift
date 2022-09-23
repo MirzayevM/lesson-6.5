64dd420f56871d6d40e08301a721cc017b99b6ae
Mirzabek <mirzabekmirzayev746@gmail.com>
2022-09-23 16:37:18 +0500

    Initial Commit

---
+++ task-6.5/task_6_5App.swift
@@ -0,0 +1,17 @@
+//
+//  task_6_5App.swift
+//  task-6.5
+//
+//  Created by Mirzabek on 23/09/22.
+//
+
+import SwiftUI
+
+@main
+struct task_6_5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
